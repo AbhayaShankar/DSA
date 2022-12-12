3f3d91a70743cd5f5d960298cea2a9267d4f5833
Abhaya <shankarabhaya9@gmail.com>
2022-12-13 00:36:58 +0530

    Assignments - conditionals basic done

---
+++ Conditionals/src/com/Abhaya/Assignments.java
@@ -0,0 +1,125 @@
+package com.Abhaya;
+
+import java.util.Scanner;
+
+public class Assignments {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //1. Area Of Circle Java Program
+//        System.out.print("Enter radius of Circle : ");
+//        double r = in.nextDouble();
+//        double areaOfCircle = 3.14 * r * r;
+//        System.out.println("Area of Circle is : " + areaOfCircle + "sq. units");
+
+
+        //2. Area Of Triangle
+//        System.out.print("Enter the height of triangle : ");
+//        double h = in.nextDouble();
+//        System.out.print("Enter the length of triangle : ");
+//        double l = in.nextDouble();
+//        double areaOfTriangle = 0.5 * h * l;
+//        System.out.println("The area of Triangle is : " + areaOfTriangle + "sq. units");
+
+        // Similar questions so skipping them, though writing the formula for them.
+        // 3. Area Of Rectangle Program
+//                area = l * b;
+
+
+        // 4. Area Of Isosceles Triangle
+//            area = A = b * h/2;
+
+        // 5. Area Of Parallelogram
+//            area = b * h;
+
+
+        // 6. Area Of Rhombus
+            // area = 0.5 * d1 * d2; // if diagonals are given
+            // area = b * h ; //if base and height are given
+
+        // 7. Area of Equilateral triangle
+
+//        System.out.print("Enter the side of Equilateral triangle : ");
+//        double side = in.nextDouble();
+//        double areaOfEquiTriangle = Math.sqrt(3) * side * side / 4;
+//        System.out.println("Area of Equilateral Triangle is : " + areaOfEquiTriangle);
+
+        // 8. Perimeter Of Circle
+//            perimeter = 2 * pi * r;
+
+        // 9. Perimeter Of Equilateral Triangle
+//            perimeter = 3 * side;
+
+        // 10. Perimeter Of Parallelogram
+//            perimeter = 2 * ( l1 + l2);
+
+        // 11. Perimeter Of Rectangle
+//            perimeter = 2 * ( l + b);
+
+        // 12. Perimeter Of Square
+//            perimeter = 4 * side;
+
+//         13. Perimeter Of Rhombus
+//            perimeter = 4 * side;
+
+
+        // 14. Volume Of Cone Java Program
+//        System.out.print("Enter the radius of Cone : ");
+//        double r = in.nextDouble();
+//        System.out.print("Enter the height of cone : ");
+//        double h = in.nextDouble();
+//        double volumeOfCone = 3.14 * r * r * h / 3;
+//        System.out.println("Volume of Cone is : " + volumeOfCone + "cubic units");
+
+        // 15. Volume Of Prism
+//            volume = different for different structures
+        // refer photo for more.
+
+        // 16. Volume Of Cylinder
+//            volume = pi * r * r * h;
+
+        // 17. Volume Of Sphere
+//            volume = 4/3 * pi * r * r * r;
+
+        // 18. Volume Of Pyramid
+//                volume = l * h * b / 3;
+
+        // 19. Curved Surface Area Of Cylinder
+//             csaCylinder = 2 * pi * r * l;
+
+        // 20. Total Surface Area Of Cube
+//            tsaCube = 6 * a * a;
+
+        // 21. Fibonacci series in Java
+//        int a = 0;
+//        int b = 1;
+//        for (int n = 0; n <= 10; n++) {
+//            int c = b;
+//            System.out.println(c);
+//            b = b + a;
+//            c = a;
+//        }
+
+        // 22. Subtract the Product and Sum of Digits of an Integer
+
+        // let number = 419 --> answer : (4 * 1 * 9) - (4 + 1 + 9) = 22;
+//        System.out.print("Enter the Number : ");
+//        int num = in.nextInt();
+//        int sum = 0 ;
+//        int prod = 1;
+//        while(num > 0){
+//            int rem = num % 10;
+//            sum = sum + rem;
+//            prod = prod * rem;
+//            num = num / 10;
+//        }
+//
+//        System.out.println("Answer is : " + ( prod - sum)); // Answer is : 22.
+
+        // 23. Input a number and print all the factors of that number (use loops).
+            
+
+
+
+    }
+}
